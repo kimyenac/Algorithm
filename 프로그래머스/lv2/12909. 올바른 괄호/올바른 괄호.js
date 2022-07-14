eb2687d41f38bb29d69fa40a5f3f68a9ac247172
kimyenac <101786858+kimyenac@users.noreply.github.com>
2022-07-14 13:20:49 +0900

    [level 2] Title: 올바른 괄호, Time: 5.25 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,19 @@
+function solution(s){
+
+    let stack = [];
+    for (let i = 0; i < s.length; i += 1) {
+        if (s[i] == "(") {
+            stack.push(s[i]);
+        } else {
+            if (stack.length == 0) {
+                return false
+            }
+            stack.pop()
+        }
+    }
+    if (stack.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
