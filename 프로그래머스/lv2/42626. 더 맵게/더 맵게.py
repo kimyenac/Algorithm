c2f745ca10b60e5a22e7f3ad77edef274d401827
kimyenac <101786858+kimyenac@users.noreply.github.com>
2022-07-14 11:38:45 +0900

    [level 2] Title: 더 맵게, Time: 1732.51 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,13 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    scoville.sort()
+    heapq.heapify(scoville)
+    while scoville[0] <= K:
+        if len(scoville) == 1:
+            return -1
+        x = heapq.heappop(scoville)
+        y = heapq.heappop(scoville)
+        heapq.heappush(scoville, x+y*2)
+        answer += 1
+    return answer
