720183c2ca1da6f2e1182a4031c153c96680cedf
kimyenac <101786858+kimyenac@users.noreply.github.com>
2022-07-14 15:07:06 +0900

    [level 2] Title: 3 x n 타일링, Time: 15.62 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -0,0 +1,11 @@
+def solution(n):
+    res = [0, 3, 11]
+    idx = int(n/2)
+    
+    if n % 2 != 0: return 0
+    if idx < 3: return res[idx]
+
+    for i in range(3, idx+1):
+        res.append((3*res[i-1]+sum(res[1:i-1])*2+2)%1000000007)
+        
+    return res[idx]
