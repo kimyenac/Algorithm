3b6ce312f68d370bc39cfe19d9086a7fdd320c59
kimyenac <101786858+kimyenac@users.noreply.github.com>
2022-07-14 13:28:46 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 1.53 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -0,0 +1,52 @@
+def rotate(array, d):
+    n = len(array)
+    result = [[0] * n for _ in range(n)]
+    if d % 4 == 1:
+        for r in range(n):
+            for c in range(n):
+                result[c][n-r-1] = array[r][c]
+    elif d % 4 == 2:
+        for r in range(n):
+            for c in range(n):
+                result[n-r-1][n-c-1] = array[r][c]
+    elif d % 4 == 3:
+        for r in range(n):
+            for c in range(n):
+                result[n-c-1][r] = array[r][c]
+    else:
+        for r in range(n):
+            for c in range(n):
+                result[r][c] = array[r][c]
+    return result
+
+def check(locks):
+    n = len(locks) // 3
+    for i in range(n, n*2):
+        for j in range(n, n*2):
+            if locks[i][j] != 1:
+                return False
+    return True
+
+def solution(key, lock):
+    m = len(key)
+    n = len(lock)
+    
+    locks = [[0] * (n * 3) for _ in range(n * 3)]
+    for i in range(n):
+        for j in range(n):
+            locks[n+i][n+j] = lock[i][j]
+            
+    for i in range(1, n*2):
+        for j in range(1, n*2):
+            for d in range(4):
+                r_key = rotate(key, d)
+                for x in range(m):
+                    for y in range(m):
+                        locks[i+x][j+y] += r_key[x][y]
+                if check(locks):
+                    return True
+                for x in range(m):
+                    for y in range(m):
+                        locks[i+x][j+y] -= r_key[x][y]
+    
+    return False
