91b3292a40c020dd188fd594716c4eabeae2a5aa
kimyenac <101786858+kimyenac@users.noreply.github.com>
2022-12-31 15:20:03 +0900

    [level 0] Title: 순서쌍의 개수, Time: 1971.94 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if (i * j == n):
+                answer += 1
+                break;
+            if (i * j > n):
+                break;
+    return answer
