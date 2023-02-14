544884c39d4ba54d459040aa75780f771965b7f7
김예나 <101786858+kimyenac@users.noreply.github.com>
2023-02-14 22:55:01 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,10 @@
+def solution(A, B):
+    answer = 0
+    for i in range(len(A)):
+        if (A == B):
+            return answer
+        A = A[-1] + A[:-1]
+        answer += 1
+    if (A == B):
+        return answer
+    return -1
