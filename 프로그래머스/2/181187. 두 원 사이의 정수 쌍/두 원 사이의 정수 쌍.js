2926238c158d61c9beb23ffa59d32de69bae4fdf
김예나 <101786858+kimyenac@users.noreply.github.com>
2026-02-13 14:59:01 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 10.82 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -0,0 +1,16 @@
+function solution(r1, r2) {
+    let answer = 0;
+    let y1 = 0, y2 = 0;
+    
+    for (let i = 1; i <= r2; i++) {
+        let y1 = Math.sqrt(Math.pow(r1, 2) - Math.pow(i, 2));
+        let y2 = Math.sqrt(Math.pow(r2, 2) - Math.pow(i, 2));
+        
+        if (isNaN(y1)) y1 = 0;
+        
+        answer += ((Math.floor(y2) - Math.ceil(y1)) + 1);
+    }
+    
+    return answer * 4;
+}
+
